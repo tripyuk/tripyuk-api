b73eefbe6b19c6426931bb9b6427caa94c469f85
okiww <budimanokky3@gmail.com>
2019-05-09 00:20:15 +0700

    feat(user-auth): add baseresponse and userresponse api

---
+++ app/Http/Responses/UserResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Http\Responses;
+
+
+class UserResponse extends BaseResponse
+{
+    public static function successLogin($data)
+    {
+        return [
+            'error' => 0,
+            'status' => 401,
+            'data' => $data,
+            'message' => 'Success login',
+        ];
+    }
+    public static function validateEmailAndPassword()
+    {
+        return [
+            'status' => 401,
+            'message' => 'These credentials do not match our records',
+        ];
+    }
+}
