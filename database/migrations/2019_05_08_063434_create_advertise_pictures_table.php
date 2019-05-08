529a5b43459642e1f411d5ec8ca1cecf8dd24691
okiww <budimanokky3@gmail.com>
2019-05-08 13:40:53 +0700

    create migration agent, advertiselisting, advertisepicture

---
+++ database/migrations/2019_05_08_063434_create_advertise_pictures_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertisePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertise_pictures', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('advertise_listing_id');
+            $table->mediumText('path');
+
+            $table->foreign('advertise_listing_id')
+                ->references('id')
+                ->on('advertise_listing')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertise_pictures');
+    }
+}
